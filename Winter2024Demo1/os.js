63da698eeb58169506750b0cf99a356bc786b8e8
jayEag <jay.eagles@Keyin.com>
2024-01-11 10:12:11 -0330

    Second Commit

---
+++ Winter2024Demo1/os.js
@@ -0,0 +1,11 @@
+var os = require('os');
+
+console.log('OS CPU Architecture: ' + os.arch());
+console.log('Free memory of the system: ' + os.freemem());
+console.log('Total memory of the system: ' + os.totalmem());
+console.log('OS Platform: ' + os.platform());
+console.log('Information about the current user: ' + os.userInfo().username);
+console.log('OS Type: ' + os.type());
+console.log('Current directory: ' + os.homedir());
+console.log('Uptime: ' + os.uptime());
+console.log('Hostname: ' + os.hostname());
